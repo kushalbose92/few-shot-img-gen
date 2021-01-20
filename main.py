921e8bb28eece316c7e46b31f71c45bf548775f4
kushalbose92 <kushalbose92@gmail.com>
2021-01-21 02:17:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,60 +15,79 @@ import copy
 import torchvision.transforms as transforms
 import learn2learn as l2l
 
-from dataloader import MNISTLoader, OmniglotLoader, CIFAR10Loader, Fashion_MNISTLoader
+# Package import
+from dataloader import MNISTLoader, OmniglotLoader, Fashion_MNISTLoader
 from train import ModelTrainer
 from eval import ModelEvaluation
 from utils import UtilFunctions
 
 
+# Input parameters
 num_shot = 5
 num_query = 5
-num_ways = 3
-alpha = 500.0
-beta = 300.0
+num_ways = 1
+alpha_val = 500.0
+beta_val = 300.0
 num_support = num_shot * num_ways
 num_test = num_query * num_ways
 num_samples = (num_shot + num_query) * num_ways
 latent_dims = 2
 num_train_tasks = 50000
 num_test_tasks = 500
+dataset = "mnist"
+use_saved_model = False
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-data_loader = OmniglotLoader(num_ways, num_shot, num_query, num_train_tasks, num_test_tasks)
 
+if dataset == "mnist":
+    # Task generation for MNIST dataset
+    data_loader = MNISTLoader(num_ways, num_shot, num_query, num_train_tasks, num_test_tasks)
+
+elif dataset == "fashion-mnist":
+    # Task generation for fashion-MNIST dataset
+    data_loader = Fashion_MNISTLoader(num_ways, num_shot, num_query, num_train_tasks, num_test_tasks)
+
+elif dataset == "omniglot":
+    # Task generation for Omniglot dataset
+    data_loader = OmniglotLoader(num_ways, num_shot, num_query, num_train_tasks, num_test_tasks)
+else:
+    print("Wrong selection of the dataset\n")
+
+
+# Task set for training and testing
 train_tasks = data_loader.train_task_set()
 test_tasks = data_loader.test_task_set()
 
-
-# alpha - Intra-Clustering Weighting Constant
-# beta -  Inter-Clustering Weighting Constant
-# If ways = 1 then put alpha = 0.0
-
+# Method for model training
 trainer = ModelTrainer(
                     train_tasks = train_tasks,
                     shots = num_shot,
                     ways = num_ways,
                     maml_lr = 0.001,
                     base_lr = 0.01,
-                    alpha = alpha,
-                    beta = beta,
+                    alpha = alpha_val,
+                    beta = beta_val,
                     meta_train_batch = 32,
                     meta_train_epochs = 1,
                     steps_per_task = 1,
                     latent_dims = latent_dims,
                     device = device)
 
-MODEL_PATH = trainer.train()
-# MODEL_PATH = os.getcwd() + "/saved_models/omniglot_3-way-5-shot.pt"
+# Saved models
+if use_saved_model == True:
+    MODEL_PATH = os.getcwd() + "/saved_model.pt"
+else:
+    MODEL_PATH = trainer.train()
 
+# method defined for model evaluation
 evaluation = ModelEvaluation(
                             test_tasks = test_tasks,
                             shots = num_shot,
                             ways = num_ways,
                             base_lr = 0.01,
-                            alpha = alpha,
-                            beta = beta,
+                            alpha = alpha_val,
+                            beta = beta_val,
                             meta_test_batch = 1,
                             meta_test_epochs = 1,
                             steps_per_task = 50,
@@ -76,4 +95,3 @@ evaluation = ModelEvaluation(
                             device = device)
 
 evaluation.eval(MODEL_PATH)
-
