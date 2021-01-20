95310c983d4e527b6ed90f04e2883d4c384e8acb
kushalbose92 <kushalbose92@gmail.com>
2021-01-21 02:16:28 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -19,7 +19,7 @@ import time
 from model import ImageGenerator
 from utils import UtilFunctions
 
-
+# Class for Model Evaluation
 class ModelEvaluation():
     def __init__(self, test_tasks, shots, ways, base_lr, alpha, beta, meta_test_batch, meta_test_epochs, steps_per_task, latent_dims, device):
         self.test_tasks = test_tasks
@@ -34,8 +34,11 @@ class ModelEvaluation():
         self.latent_dims = latent_dims
         self.device = device
 
+    # Evaluation stage
     def eval(self, MODEL_PATH):
         img_gen = ImageGenerator(self.latent_dims, self.ways, self.shots)
+
+        # Loading the saved model from the directory
         img_gen.load_state_dict(torch.load(MODEL_PATH))
         img_gen.to(self.device)
         img_gen_meta = l2l.algorithms.MAML(img_gen, lr = self.base_lr, first_order = True)
@@ -44,6 +47,8 @@ class ModelEvaluation():
 
         avg_recon_loss = 0.0
         total_fid = 0.0
+
+        # Outer loop
         for i in range(self.meta_test_epochs):
 
             query_recon_loss = 0.0
@@ -55,7 +60,6 @@ class ModelEvaluation():
                 data = data.to(self.device)
                 labels = labels.to(self.device)
 
-                # img_gen_learner.train()
 
                 adaptation_indices = np.zeros(data.size(0), dtype=bool)
                 adaptation_indices[np.arange(self.shots * self.ways)*2] = True
@@ -68,61 +72,63 @@ class ModelEvaluation():
                 inter_loss_list = []
                 intra_loss_list = []
 
-                print("Reconstruction \t Inter-Clustering Loss \t Intra-Clustering Loss\n")
+                print("Reconstruction Loss \t Inter-Clustering Loss \t Intra-Clustering Loss \n")
+
+                # Inner loop
                 for step in range(self.steps_per_task):
+
+                    # Support set as input to the model
                     support_recon, latent_proto, latent_radius, latent_feat = img_gen_learner(support_set, gamma = 1.0)
 
                     recon_loss = UtilFunctions().loss_reconstruction(support_set, support_recon)
                     inter_clustering_loss = UtilFunctions().radius_loss_interclass(latent_proto, latent_radius) * self.beta
                     intra_clustering_loss = UtilFunctions().radius_loss_intraclass(latent_proto, latent_radius, self.shots, self.ways) * self.alpha
+                    
                     recon_loss_list.append(recon_loss.item())
                     inter_loss_list.append(inter_clustering_loss.item())
                     intra_loss_list.append(intra_clustering_loss.item())
 
-                    train_loss = recon_loss + inter_clustering_loss + intra_clustering_loss 
+                    train_loss = recon_loss + inter_clustering_loss + intra_clustering_loss
                     train_loss /= len(adaptation_indices)
                     img_gen_learner.adapt(train_loss)
                     print("Step  ", step+1, " ", recon_loss.item(), " ", inter_clustering_loss.item(), " ", intra_clustering_loss.item())
 
-                # img_gen_learner.eval()
+
+                # Query set given to model
                 query_recon, latent_proto, latent_radius, latent_feat = img_gen_learner(query_set, gamma = 1.0)
-                # query_recon_1, latent_proto_1, latent_radius_1, latent_feat_1 = img_gen_learner(query_set, gamma = 0.8)
-                # query_recon_2, latent_proto_2, latent_radius_2, latent_feat_2 = img_gen_learner(query_set, gamma = 0.2)
-                # query_recon_3, latent_proto_3, latent_radius_3, latent_feat_3 = img_gen_learner(query_set, gamma = -0.2)
-                # query_recon_4, latent_proto_4, latent_radius_4, latent_feat_4 = img_gen_learner(query_set, gamma = -0.8)
-                  
+
 
                 recon_loss = UtilFunctions().loss_reconstruction(query_set, query_recon)
                 inter_clustering_loss = UtilFunctions().radius_loss_interclass(latent_proto, latent_radius) * self.beta
                 intra_clustering_loss = UtilFunctions().radius_loss_intraclass(latent_proto, latent_radius, self.shots, self.ways) * self.alpha
 
-                # print("Task No. ", t_idx+1, "\t", recon_loss.item(), "\t", inter_clustering_loss.item(), "\t", intra_clustering_loss.item(), "\n" )
+                print("Task No. ", t_idx+1, "\t", recon_loss.item(), "\t", inter_clustering_loss.item(), "\t", intra_clustering_loss.item(), "\n" )
 
                 query_loss = recon_loss + inter_clustering_loss + intra_clustering_loss
                 query_loss /= len(query_set)
                 query_recon_loss += query_loss
 
                 # Generated Image Visualization
-                # UtilFunctions().image_visual(query_set, query_recon, self.shots)
-             
-                # Generated Point Visualization
-                # UtilFunctions().class_distribution_visual(latent_proto, latent_radius)
-              
-                # Plot of the Loss Functions 
+                UtilFunctions().image_visual(query_set, query_recon, self.shots)
+
+                # Generated Point Visualization in 2D space
+                UtilFunctions().neighborhood_visual(latent_proto, latent_radius)
+
+                # Plot of the three Loss Functions
                 UtilFunctions().loss_plot(recon_loss_list, inter_loss_list, intra_loss_list)
 
-                # FID score Estimation
+                # FID score estimation
                 fid_score = UtilFunctions().calculate_fid(query_set, query_recon, self.shots)
-              
+
 
                 total_fid += fid_score
-                print("FID ", fid_score)
+                print("FID score:  ", fid_score)
 
-            # query_recon_loss /= self.meta_test_batch
             print(i+1, " Query Reconstruction Loss: ", query_recon_loss.item())
 
             avg_recon_loss += query_recon_loss
 
         avg_recon_loss = avg_recon_loss / self.meta_test_epochs
+
         print("\nAverage Reconstruction Loss:  ", avg_recon_loss.item())
         print("Average FID score  ", (total_fid / self.meta_test_epochs))
